1735b7203d1c751e0cfd1c73a771190164cb801a
Jeffin <jmathew330@gmail.com>
2024-11-19 15:11:55 -0500

    Updated index.js

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,5 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+
+console.log(initialCards);
