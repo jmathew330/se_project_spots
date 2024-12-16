f85fff53a69fc4d4c53651712d4d04cba9c7145d
Jeffin <jmathew330@gmail.com>
2024-12-16 18:40:22 -0500

    Add functionality to create new post

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//cards array
 const initialCards = [
   {
     name: "Val Thorens",
@@ -24,22 +25,33 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
-
+//profile section
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileAddButton = document.querySelector(".profile__add-btn");
 const profileName = document.querySelector(".profile__name");
 const descriptionName = document.querySelector(".profile__description");
 
+//edit profile modal
 const editModal = document.querySelector("#edit-modal");
-const editFormElement = editModal.querySelector(".modal__form");
+const editForm = editModal.querySelector(".modal__form");
 const editModalCloseButton = editModal.querySelector(".modal__close-btn");
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
 );
 
+//add card modal
+const cardModal = document.querySelector("#add-card-modal");
+const cardForm = cardModal.querySelector(".modal__form");
+const cardModalCloseButton = cardModal.querySelector(".modal__close-btn");
+const cardNameInput = cardModal.querySelector("#add-card-name-input");
+const cardLinkInput = cardModal.querySelector("#add-card-link-input");
+
+//cards
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
 
+//function to create card HTML
 function getCardElement(data) {
   const cardElement = cardTemplate.content
     .querySelector(".card")
@@ -54,28 +66,56 @@ function getCardElement(data) {
   return cardElement;
 }
 
-function openModal() {
-  editModalNameInput.value = profileName.textContent;
-  editModalDescriptionInput.value = descriptionName.textContent;
-  editModal.classList.add("modal_opened");
+//forEach loop to add card elements from array to DOM
+initialCards.forEach((item) => {
+  const cardElement = getCardElement(item);
+  cardsList.prepend(cardElement);
+});
+
+//function to open modal
+function openModal(modal) {
+  modal.classList.add("modal_opened");
 }
 
-function closeModal() {
-  editModal.classList.remove("modal_opened");
+//function to close modal
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
 }
 
+//function for edit modal form submission
 function handleEditFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = editModalNameInput.value;
   descriptionName.textContent = editModalDescriptionInput.value;
-  closeModal();
+  closeModal(editModal);
 }
 
-profileEditButton.addEventListener("click", openModal);
-editModalCloseButton.addEventListener("click", closeModal);
-editFormElement.addEventListener("submit", handleEditFormSubmit);
-
-for (let i = 0; i < initialCards.length; i++) {
-  const cardElement = getCardElement(initialCards[i]);
+//function for add card modal form submission
+function handleAddCardSubmit(evt) {
+  evt.preventDefault();
+  const inputValues = { name: cardNameInput.value, link: cardLinkInput.value };
+  const cardElement = getCardElement(inputValues);
   cardsList.prepend(cardElement);
+  closeModal(cardModal);
 }
+//edit profile event listener on click
+profileEditButton.addEventListener("click", () => {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescriptionInput.value = descriptionName.textContent;
+  openModal(editModal);
+});
+//edit profile close button event listener on click
+editModalCloseButton.addEventListener("click", () => {
+  closeModal(editModal);
+});
+//add card button event listener on click
+profileAddButton.addEventListener("click", () => {
+  openModal(cardModal);
+});
+//add card close button event listener on click
+cardModalCloseButton.addEventListener("click", () => {
+  closeModal(cardModal);
+});
+//event listeners for edit profile and add card on submission
+editForm.addEventListener("submit", handleEditFormSubmit);
+cardForm.addEventListener("submit", handleAddCardSubmit);
